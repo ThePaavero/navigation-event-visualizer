9cc5f0b7b5636754706c85f8157602d2075968c8
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2019-03-23 22:31:35 +0200

    Opacity change for clicked link, config, etc.

--- navigation-presentation.js
+++ navigation-presentation.js
@@ -1,11 +1,27 @@
 (() => {
 
+  const config = {
+    changeLinkOpacity: true,
+    preventFurtherClicks: true,
+  }
+
+  const visualizeLoading = e => {
+    const link = e.currentTarget
+    if (config.changeLinkOpacity) {
+      link.style.opacity = 0.3
+    }
+    if (config.preventFurtherClicks) {
+      link.addEventListener('click', e => {
+        e.preventDefault()
+      })
+    }
+  }
+
+
   const init = () => {
     const linksWithNavigationEffect = document.querySelectorAll('a, button')
     Array.from(linksWithNavigationEffect).forEach(link => {
-      link.addEventListener('click', e => {
-        console.log('Clicked!')
-      })
+      link.addEventListener('click', visualizeLoading)
     })
   }
 
