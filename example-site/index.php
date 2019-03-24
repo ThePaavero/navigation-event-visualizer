10a82bbaa2be5030626f05676b244abb113e0599
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2019-03-24 09:24:45 +0200

    Styles, mostly.

--- example-site/index.php
+++ example-site/index.php
@@ -21,6 +21,12 @@ $text = isset($dummyPagesToDummyContent[$targetDummyPage]) ? $dummyPagesToDummyC
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Demo</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      font-size: 16px;
+    }
+  </style>
 </head>
 <body>
 <header>
